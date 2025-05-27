4afca6ddef704c93a4a7389c4b7a26b21e24e14a
Yu-Jin9 <ujh50188@gmail.com>
2025-05-27 14:10:27 +0900

    chore: 초기설정

---
+++ src/main/java/com/day_walk/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.day_walk.backend.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.day_walk.backend.domain.controller"})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "하루걸음 Project",
+                description = "하루걸음 backend api 명세서",
+                version = "1.0.0"
+        )
+)
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
---
+++ src/test/java/com/day_walk/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day_walk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
