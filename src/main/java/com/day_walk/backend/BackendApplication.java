55da7b48fb02c484fcd4c7cefc09a0d8f98323a6
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-05-28 10:10:58 +0900

    chore: BaseEntity 설정

--- src/main/java/com/day_walk/backend/BackendApplication.java
+++ src/main/java/com/day_walk/backend/BackendApplication.java
@@ -2,13 +2,13 @@ package com.day_walk.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-		System.out.println("Final Project Start!!");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+        System.out.println("Final Project Start!!");
+    }
 }
