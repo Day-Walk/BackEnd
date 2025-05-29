a2f58ecaa8ac2e577531c349a20b78b143f7401f
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-05-29 22:51:15 +0900

    chore: cors 설정

--- src/main/java/com/day_walk/backend/global/config/WebSecurityConfig.java
+++ src/main/java/com/day_walk/backend/global/config/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173", "https://day-walk.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
