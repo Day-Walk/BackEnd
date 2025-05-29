b25561f593e1d327e5a59f11109e83490991998f
Yu-Jin9 <ujh50188@gmail.com>
2025-05-29 15:20:24 +0900

    feat: 코스 상세조회 기능 초기구현 완료

--- src/main/java/com/day_walk/backend/domain/course/controller/CourseController.java
+++ src/main/java/com/day_walk/backend/domain/course/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.day_walk.backend.domain.course.controller;
 
 import com.day_walk.backend.domain.course.data.dto.in.SaveCourseDto;
+import com.day_walk.backend.domain.course.data.dto.out.GetCourseDto;
 import com.day_walk.backend.domain.course.service.CourseService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +29,18 @@ public class CourseController {
         response.put("courseId", courseId);
         response.put("message", courseId == null ? "코스 저장 실패.." : "코스 저장 성공!");
         response.put("success", courseId != null);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getCourse(@RequestParam("courseId") UUID courseId) {
+        GetCourseDto courseInfo = courseService.getCourse(courseId);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", courseInfo != null);
+        response.put("message", courseInfo == null ? "코스 상세조회 실패.." : "코스 상세조회 성공!");
+        response.put("courseInfo", courseInfo);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
--- src/main/java/com/day_walk/backend/domain/course/service/CourseService.java
+++ src/main/java/com/day_walk/backend/domain/course/service/CourseService.java
@@ -1,13 +1,24 @@
 package com.day_walk.backend.domain.course.service;
 
+import com.day_walk.backend.domain.category.bean.GetCategoryEntityBean;
+//import com.day_walk.backend.domain.category.data.CategoryEntity;
+import com.day_walk.backend.domain.course.bean.GetCourseEntityBean;
 import com.day_walk.backend.domain.course.bean.SaveCourseEntityBean;
 import com.day_walk.backend.domain.course.data.CourseEntity;
 import com.day_walk.backend.domain.course.data.dto.in.SaveCourseDto;
+import com.day_walk.backend.domain.course.data.dto.out.GetCourseDto;
+import com.day_walk.backend.domain.place.bean.GetPlaceEntityBean;
+//import com.day_walk.backend.domain.place.data.PlaceEntity;
+import com.day_walk.backend.domain.place.data.out.GetPlaceDto;
+import com.day_walk.backend.domain.sub_category.bean.GetSubCategoryEntityBean;
+//import com.day_walk.backend.domain.sub_category.data.SubCategoryEntity;
 import com.day_walk.backend.domain.user.bean.GetUserEntityBean;
 import com.day_walk.backend.domain.user.data.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -32,5 +43,34 @@ public class CourseService {
         return course == null ? null : course.getId();
     }
 
+    private final GetCourseEntityBean getCourseEntityBean;
+    private final GetPlaceEntityBean getPlaceEntityBean;
+    private final GetSubCategoryEntityBean getSubCategoryEntityBean;
+    private final GetCategoryEntityBean getCategoryEntityBean;
 
+    public GetCourseDto getCourse(UUID courseId) {
+        CourseEntity courseEntity = getCourseEntityBean.exec(courseId);
+        if((courseEntity == null)&& !courseEntity.isVisible() && !courseEntity.isHasDelete()) return null;
+        UserEntity userEntity = getUserEntityBean.exec(courseEntity.getUserId());
+        List<GetPlaceDto> getPlaceDtoList = new ArrayList<>();
+//        for (UUID placeId : courseEntity.getPlaceList()) {
+//            PlaceEntity placeEntity = getPlaceEntityBean.exec(placeId);
+//            SubCategoryEntity subCategory = getSubCategoryEntityBean.exec(placeEntity.getSubCategoryId());
+//            CategoryEntity category = getCategoryEntityBean.exec(subCategory.getCategoryId());
+//            GetPlaceDto getPlaceDto = GetPlaceDto.builder()
+//                    .place(placeEntity)
+//                    .category(category.getName())
+//                    .subCategory(subCategory.getName())
+//                    .build();
+//            getPlaceDtoList.add(getPlaceDto);
+//        }
+
+        GetCourseDto courseInfo = GetCourseDto.builder()
+                .userName(userEntity.getName())
+                .title(courseEntity.getTitle())
+//                .placeList(getPlaceDtoList)
+                .build();
+
+        return courseInfo == null ? null : courseInfo;
+    }
 }
