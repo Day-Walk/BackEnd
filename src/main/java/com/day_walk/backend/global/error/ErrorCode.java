03301d98da3a7e513c9f5eae554b634fe2ecaf89
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-06-02 09:54:16 +0900

    feat: error code 변경

--- src/main/java/com/day_walk/backend/global/error/ErrorCode.java
+++ src/main/java/com/day_walk/backend/global/error/ErrorCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, false, "USER_001", "유저를 찾을 수 없습니다."),
-    USER_AGE_BAD_REQUEST(HttpStatus.BAD_REQUEST, false, "USER_002", "유저의 나이 입력이 잘못되었습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, false, "USER_010", "유저를 찾을 수 없습니다."),
+    USER_AGE_BAD_REQUEST(HttpStatus.BAD_REQUEST, false, "USER_020", "유저의 나이 입력이 잘못되었습니다.");
 
     private final HttpStatus httpStatus;
     private final boolean success;
