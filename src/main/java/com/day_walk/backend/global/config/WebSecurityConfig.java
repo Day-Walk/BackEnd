07bead28784b7a3cae4d1b6a25381673d96e5f66
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-06-18 12:47:50 +0900

    feat: cors 추가

--- src/main/java/com/day_walk/backend/global/config/WebSecurityConfig.java
+++ src/main/java/com/day_walk/backend/global/config/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173", "https://final-front-end-fawn.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
