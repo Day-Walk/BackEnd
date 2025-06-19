42b6458b90fa9ab59ec784029de98b54a9de7f4b
Yu-Jin9 <ujh50188@gmail.com>
2025-06-19 15:52:19 +0900

    feat: 에러 코드 추가

--- src/main/java/com/day_walk/backend/domain/click_log/service/ClickLogService.java
+++ src/main/java/com/day_walk/backend/domain/click_log/service/ClickLogService.java
@@ -1,6 +1,8 @@
 package com.day_walk.backend.domain.click_log.service;
 
 import com.day_walk.backend.domain.click_log.data.dto.in.SaveClickLogDto;
+import com.day_walk.backend.global.error.CustomException;
+import com.day_walk.backend.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +25,7 @@ public class ClickLogService {
     public SaveClickLogDto saveClickLog(SaveClickLogDto saveClickLogDto) {
         String uri = UriComponentsBuilder
                 .fromUriString(ELK_SERVER_URI)
-                .pathSegment( "click-log")
+                .pathSegment("click-log")
                 .toUriString();
 
         HttpHeaders headers = new HttpHeaders();
@@ -44,7 +46,7 @@ public class ClickLogService {
 
             if (response.getBody() == null) {
                 log.warn("ELK 응답 body가 null입니다. 요청 URI: {}", uri);
-                return null;
+                throw new CustomException(ErrorCode.ELK_SERVER_ERROR);
             }
 
             return response.getBody();
