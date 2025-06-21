da31befaf235333ad21d9b8214827d3836d0d634
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-06-21 23:36:30 +0900

    hotfix: cors 설정 추가

--- src/main/java/com/day_walk/backend/global/config/WebSecurityConfig.java
+++ src/main/java/com/day_walk/backend/global/config/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173", "https://final-front-end-fawn.vercel.app", "https://day-walk.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173", "https://final-front-end-fawn.vercel.app", "https://day-walk.vercel.app", "https://hktoss.shop"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
