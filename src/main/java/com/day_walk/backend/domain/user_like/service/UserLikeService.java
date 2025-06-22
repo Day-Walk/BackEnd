fac03403a888b42b95cd51dca846adf459d6a5cb
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-06-22 21:59:20 +0900

    remove: 불필요한 패키지 제거

--- src/main/java/com/day_walk/backend/domain/user_like/service/UserLikeService.java
+++ src/main/java/com/day_walk/backend/domain/user_like/service/UserLikeService.java
@@ -5,14 +5,12 @@ import com.day_walk.backend.domain.user.data.UserEntity;
 import com.day_walk.backend.domain.user_like.bean.SaveUserLikeEntityBean;
 import com.day_walk.backend.domain.user_like.bean.GetUserLikeEntityBean;
 import com.day_walk.backend.domain.user_like.data.UserLikeEntity;
-import com.day_walk.backend.domain.user_like.data.dto.in.SaveCategoryListDto;
-import com.day_walk.backend.domain.user_like.data.dto.in.SaveUserLikeDto;
+import com.day_walk.backend.domain.user_like.data.in.SaveUserLikeDto;
 import com.day_walk.backend.global.error.CustomException;
 import com.day_walk.backend.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,11 +32,11 @@ public class UserLikeService {
 
         List<UserLikeEntity> userLikeEntityList = saveUserLikeDto.getCategoryList().stream()
                 .map(saveCategoryListDto -> UserLikeEntity.builder()
-                            .id(UUID.randomUUID())
-                            .userId(userEntity.getId())
-                            .categoryId(saveCategoryListDto.getCategoryId())
-                            .tagList(saveCategoryListDto.getTagList())
-                            .build())
+                        .id(UUID.randomUUID())
+                        .userId(userEntity.getId())
+                        .categoryId(saveCategoryListDto.getCategoryId())
+                        .tagList(saveCategoryListDto.getTagList())
+                        .build())
                 .toList();
 
         saveUserLikeEntityBean.exec(userLikeEntityList);
